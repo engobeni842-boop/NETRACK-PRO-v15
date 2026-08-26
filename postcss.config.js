ba0b1963b685c27e4f16bcea6fdbb760dd977be0
Emmanuel Musiyiwa <engobeni842@gmail.com>
2026-08-26 13:25:25 +0200

    fix postcss config, stop tracking node_modules

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
   },
-=======
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
->>>>>>> 897a1bab5625ac3688a904f98344895b392ead82
 }
